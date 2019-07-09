be460b89bb8effa034403e7bd7492af3d76365e8
Miles Manning <mileslmanning@gmail.com>
2019-07-09 18:18:17 -0500

    first commit

---
+++ client/src/components/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+export default function App(props) {
+  var content = (
+    <div>
+      <h1>
+        Hello World
+      </h1>
+    </div>
+  );
+  return content; 
+};
